b5f554a730fcb7dfb38cad48d3cc8cbc67049a5c
unknown <noah.rissatii@gmail.com>
2023-11-13 19:08:10 -0300

    feat: criando teste para registrar contato com todos os campos corretos

--- testeSelenium/src/test/java/pages/RegisterPageTest.java
+++ testeSelenium/src/test/java/pages/RegisterPageTest.java
@@ -1,17 +1,35 @@
 package pages;
 
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import static org.junit.Assert.assertThat;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegisterPageTest {
-
+    private WebDriver driver;
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new FirefoxDriver();
+    }
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
     @Test
     @DisplayName("Should open and close firefox browser")
     void shouldOpenAndCloseChromeBrowser() throws InterruptedException {
@@ -21,11 +39,22 @@ public class RegisterPageTest {
         Thread.sleep(1000);
         driver.quit();
     }
+
     @Test
     @DisplayName("Should register a new contact with name, email and phone completed")
-     public void registerContact() {
-    SignInPage signInPage = new SignInPage(driver);
-    HomePage homePage = signInPage.loginValidUser("userName", "password");
-    assertThat(homePage.getMessageText(), is("Hello userName"));
-  }
+    public void registerContact() {
+        Faker faker = new Faker();
+
+        String name = faker.name().fullName();
+        String phone = faker.phoneNumber().toString();
+        String email = faker.internet().emailAddress();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.registerNewContact(name,email,phone);
+        final Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
+        final String alertText = alert.getText();
+        alert.accept();
+        assertEquals("Contato salvo com sucesso!", alertText);
+    }
 }
