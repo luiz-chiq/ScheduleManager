839b31247261f117c4eee37e25e5e14c023c9944
unknown <noah.rissatii@gmail.com>
2023-11-13 18:15:14 -0300

    feat: criando m

--- testeSelenium/src/test/java/pages/RegisterPage.java
+++ testeSelenium/src/test/java/pages/RegisterPage.java
@@ -7,7 +7,23 @@ public class RegisterPage {
     protected WebDriver driver;
 
     private By nameBy = By.id("name");
-    private By emaildBy = By.id("email");
+    private By emailBy = By.id("email");
     private By phoneBy = By.id("phone");
     private By buttonBy = By.cssSelector("input[type=\"submit\"][value=\"Salvar\"]");
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+        if (!driver.getCurrentUrl().equals("http://127.0.0.1:5500/src/register.html")) {
+            throw new IllegalStateException("Essa nao é a página de cadastro" +
+                    " a página atual é: " + driver.getCurrentUrl());
+        }
+    }
+
+    public RegisterPage registerNewContact(String name, String email, String phone) {
+        driver.findElement(nameBy).sendKeys(name);
+        driver.findElement(emailBy).sendKeys(email);
+        driver.findElement(phoneBy).sendKeys(phone);
+        driver.findElement(buttonBy).click();
+        return new RegisterPage(driver);
+    }
 }
