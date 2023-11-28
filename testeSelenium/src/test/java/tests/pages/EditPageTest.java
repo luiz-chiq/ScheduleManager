8c2c69e45e3f1b2e18aa9ed7c58acc6df33262d9
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-11-28 17:52:11 -0300

    refactor: altera lógica da função de acessar edição

--- testeSelenium/src/test/java/tests/pages/EditPageTest.java
+++ testeSelenium/src/test/java/tests/pages/EditPageTest.java
@@ -1,8 +1,12 @@
 package tests.pages;
 
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,12 +19,13 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EditPageTest {
     private WebDriver driver;
 
     private static final String INDEX_URL = "http://127.0.0.1:5500/index.html";
     private static final String EDIT_URL = "http://127.0.0.1:5500/edit.html";
-    private static final int CONTACT_ID = 1;
 
     @BeforeEach
     void setUp() {
@@ -36,14 +41,11 @@ public class EditPageTest {
     private void openIndexAndClickEdit() {
         driver.get(INDEX_URL);
 
-        String seletorCss = "#contacts .item#" + CONTACT_ID;
+        WebElement editButton = driver.findElement(By.xpath("//button[contains(text(), 'Editar')]"));
 
-        WebElement item = driver.findElement(By.cssSelector(seletorCss));
+        editButton.click();
 
-        if (item != null) {
-            WebElement editButton = item.findElement(By.xpath("//button[contains(text(), 'Editar')]"));
-            editButton.click();
-        }
-
+        new WebDriverWait(driver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.urlToBe(EDIT_URL));
     }
 }
