fcb8420e25077dc45eade4339d19837f471ce080
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-11-28 16:18:27 -0300

    feat: cria métodos beforeEach e afterEach para testes da página de edição

--- testeSelenium/src/test/java/pages/EditPageTest.java
+++ testeSelenium/src/test/java/pages/EditPageTest.java
@@ -1,4 +1,22 @@
 package pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class EditPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
 }
