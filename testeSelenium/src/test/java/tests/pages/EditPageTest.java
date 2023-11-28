2c8bdeffce1a3636802bedabd6a20b0d089c93c4
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-11-28 18:12:55 -0300

    refactor: coloca testes de nome em nested class

--- testeSelenium/src/test/java/tests/pages/EditPageTest.java
+++ testeSelenium/src/test/java/tests/pages/EditPageTest.java
@@ -2,10 +2,7 @@ package tests.pages;
 
 import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -47,6 +44,81 @@ public class EditPageTest {
                 .until(ExpectedConditions.urlToBe(EDIT_URL));
     }
 
+    @Nested
+    @DisplayName("Name Validation Tests")
+    class NameValidationTests {
+        @Test
+        @DisplayName("Should alert an error as name field is empty")
+        public void editContactWithEmptyName() {
+            openIndexAndClickEdit();
+
+            Faker faker = new Faker();
+            EditPage editPage = new EditPage(driver);
+
+            String name = "";
+            String phone = "+5516993388338";
+            String email = faker.internet().emailAddress();
+
+
+            editPage.editContact(name, email, phone);
+
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            System.out.println(alertText);
+            alert.accept();
+
+            assertEquals("Nome inválido!", alertText);
+        }
+
+        @Test
+        @DisplayName("Should alert an error as name field is only space characters")
+        public void editContactWithSpaceOnlyName() {
+            openIndexAndClickEdit();
+
+            Faker faker = new Faker();
+            EditPage editPage = new EditPage(driver);
+
+            String name = "   ";
+            String phone = "+5516993388338";
+            String email = faker.internet().emailAddress();
+
+
+            editPage.editContact(name, email, phone);
+
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            System.out.println(alertText);
+            alert.accept();
+
+            assertEquals("Nome inválido!", alertText);
+        }
+
+        @Test
+        @DisplayName("Should alert an error as name field has invalid characters")
+        public void editContactWithInvalidCharactersName() {
+            openIndexAndClickEdit();
+
+            Faker faker = new Faker();
+            EditPage editPage = new EditPage(driver);
+
+            String name = "-**^%$$ d4%";
+            String phone = "+5516993388338";
+            String email = faker.internet().emailAddress();
+
+
+            editPage.editContact(name, email, phone);
+
+            try {
+                Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
+                        .until(ExpectedConditions.alertIsPresent());
+                String alertText = alert.getText();
+                System.out.println(alertText);
+                alert.accept();
+            } catch (TimeoutException e) {
+                fail("The expected alert error message has not been displayed.");
+            }
+        }
+    }
     @Test
     @DisplayName("Should alert an error as email is not in correct format")
     public void editContactWithIncorrectEmailFormat() {
@@ -94,75 +166,5 @@ public class EditPageTest {
         assertEquals("Telefone inválido!", alertText);
     }
 
-    @Test
-    @DisplayName("Should alert an error as name field is empty")
-    public void editContactWithEmptyName() {
-        openIndexAndClickEdit();
 
-        Faker faker = new Faker();
-        EditPage editPage = new EditPage(driver);
-
-        String name = "";
-        String phone = "+5516993388338";
-        String email = faker.internet().emailAddress();
-
-
-        editPage.editContact(name, email, phone);
-
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        System.out.println(alertText);
-        alert.accept();
-
-        assertEquals("Nome inválido!", alertText);
-    }
-
-    @Test
-    @DisplayName("Should alert an error as name field is only space characters")
-    public void editContactWithSpaceOnlyName() {
-        openIndexAndClickEdit();
-
-        Faker faker = new Faker();
-        EditPage editPage = new EditPage(driver);
-
-        String name = "   ";
-        String phone = "+5516993388338";
-        String email = faker.internet().emailAddress();
-
-
-        editPage.editContact(name, email, phone);
-
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        System.out.println(alertText);
-        alert.accept();
-
-        assertEquals("Nome inválido!", alertText);
-    }
-
-    @Test
-    @DisplayName("Should alert an error as name field has invalid characters")
-    public void editContactWithInvalidCharactersName() {
-        openIndexAndClickEdit();
-
-        Faker faker = new Faker();
-        EditPage editPage = new EditPage(driver);
-
-        String name = "-**^%$$ d4%";
-        String phone = "+5516993388338";
-        String email = faker.internet().emailAddress();
-
-
-        editPage.editContact(name, email, phone);
-
-        try {
-            Alert alert = new WebDriverWait(driver, Duration.ofSeconds(5))
-                    .until(ExpectedConditions.alertIsPresent());
-            String alertText = alert.getText();
-            System.out.println(alertText);
-            alert.accept();
-        } catch (TimeoutException e) {
-            fail("The expected alert error message has not been displayed.");
-        }
-    }
 }
