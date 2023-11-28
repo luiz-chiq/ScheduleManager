e4b02d95b86a1acf75b66ea6b42c8fecc529b60d
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-11-28 17:44:51 -0300

    feat: cria função para acessar edição de um determinado contato

--- testeSelenium/src/test/java/tests/pages/EditPageTest.java
+++ testeSelenium/src/test/java/tests/pages/EditPageTest.java
@@ -3,11 +3,24 @@ package tests.pages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tests.Contact;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
 
 public class EditPageTest {
     private WebDriver driver;
+
+    private static final String INDEX_URL = "http://127.0.0.1:5500/index.html";
+    private static final String EDIT_URL = "http://127.0.0.1:5500/edit.html";
+    private static final int CONTACT_ID = 1;
 
     @BeforeEach
     void setUp() {
@@ -18,5 +31,19 @@ public class EditPageTest {
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void openIndexAndClickEdit() {
+        driver.get(INDEX_URL);
+
+        String seletorCss = "#contacts .item#" + CONTACT_ID;
+
+        WebElement item = driver.findElement(By.cssSelector(seletorCss));
+
+        if (item != null) {
+            WebElement editButton = item.findElement(By.xpath("//button[contains(text(), 'Editar')]"));
+            editButton.click();
+        }
+
     }
 }
